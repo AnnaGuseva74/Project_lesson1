f9040b377a461c8659600489e2cd8e25d3b06ea4
AnnGuseva <lmlka@mail.ru>
2023-02-19 18:16:03 +0500

    HomeTask2

--- src/main/java/Lesson2/HomeWork2.java
+++ src/main/java/Lesson2/HomeWork2.java
@@ -2,28 +2,26 @@ package Lesson2;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        System.out.println(sumAndCheckIncludeInDiapazon(10,2));
+        System.out.println(isSumBetween10and20(10,2));
         isPositiveOrNegative (255);
         System.out.println(isNegative (-562));
         printWordNTimes("homework", 15 );
     }
-
-    public static boolean sumAndCheckIncludeInDiapazon(int a, int b){
+    public static boolean isSumBetween10and20(int a, int b){
         int sum = a + b;
         return sum >= 10 && sum <= 20;
     }
-
     public static void isPositiveOrNegative (int i){
         if (i >=0) {System.out.println("Число положительное");}
         else {System.out.println("Число отрицательное");}
     }
-
     public static boolean isNegative (int y) {
         return y < 0;
     }
     public static void printWordNTimes(String str, int n) {
-        for (int i = 1; i <= n; i++) {
-            System.out.println("[" + i + "]" + " " + str);}
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i + " " + str);
+        }
     }
 }
 
